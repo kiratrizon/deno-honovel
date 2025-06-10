1207e7a6d1667d725a685aa46080b0e823e3f0a7
fdc-genesis <genesistroy.fdc@gmail.com>
2025-06-10 18:23:59 +0800

    revert route

--- routes/web.ts
+++ routes/web.ts
@@ -1,10 +1,10 @@
 import Route from "Illuminate/Support/Facades/Route";
 import UserController from "../app/Http/Controllers/UserController.ts";
 
-// Route.get("/", [UserController, "view"]);
+Route.get("/", [UserController, "view"]);
 
 Route.middleware([]).group(() => {
-  Route.resource('users', UserController);
+  Route.resource('users', UserController).whereNumber('user');
   Route.get("/{id}", [UserController, "test"]).whereNumber('id');
 })
 export default Route;
