855eb37ec1cab06df7813125c8ac1e06bcbda751
kyuuukira <genesistroy.fdc@gmail.com>
2025-06-12 18:12:26 +0800

    test

--- routes/web.ts
+++ routes/web.ts
@@ -1,10 +1,8 @@
 import Route from "Illuminate/Support/Facades/Route";
 import UserController from "../app/Http/Controllers/UserController.ts";
+import AdminController from "../app/Http/Controllers/AdminController.ts";
 
 Route.get("/", [UserController, "view"]);
 
-Route.middleware([]).group(() => {
-  Route.resource('users', UserController).whereNumber('user');
-  Route.get("/{id}", [UserController, "test"]).whereNumber('id');
-})
+Route.resource("admins", AdminController);
 export default Route;
