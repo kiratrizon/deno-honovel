c49aa6ec7c883602f449daba83003d0409ca20fc
kyuuukira <genesistroy.fdc@gmail.com>
2025-06-27 16:02:18 +0800

    fix 2

--- config/logging.ts
+++ config/logging.ts
@@ -1,4 +1,4 @@
-import { LogConfig } from "../vendor/honovel/framework/src/@hono-types/declaration/imain.d.ts";
+import { LogConfig } from "../vendor/honovel/@types/declaration/imain.d.ts";
 
 export default {
   default: env("LOG_CHANNEL", "stack"),
