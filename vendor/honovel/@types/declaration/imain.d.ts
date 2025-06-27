b5c995134765b7f222779dabec063e921d1db1d7
kyuuukira <genesistroy.fdc@gmail.com>
2025-06-27 15:54:50 +0800

    final

---
+++ vendor/honovel/@types/declaration/imain.d.ts
@@ -0,0 +1,71 @@
+import { Hono } from "hono";
+import { HonoSession } from "../../framework/src/hono/Http/HonoSession.ts";
+
+type SessionDataTypes = {
+  id: string;
+};
+
+// for Context
+export type Variables = {
+  httpHono: HttpHono;
+  session: HonoSession<SessionDataTypes>;
+  from_web: boolean;
+  subdomain: Record<string, string | null>;
+};
+
+export type HonoType = Hono<{
+  Variables: Variables;
+}>;
+
+export interface CorsConfig {
+  paths?: string[];
+  allowed_methods?: string[];
+  allowed_origins: string[] | null;
+  allowed_origins_patterns?: string[];
+  allowed_headers?: string[];
+  exposed_headers?: string[];
+  max_age?: number;
+  supports_credentials?: boolean;
+}
+
+interface ChannelBase {
+  driver: string;
+}
+
+interface SingleChannel extends ChannelBase {
+  driver: "single";
+  path: string;
+}
+
+interface DailyChannel extends ChannelBase {
+  driver: "daily";
+  path: string;
+  days: number;
+}
+
+interface StackChannel extends ChannelBase {
+  driver: "stack";
+  channels: string[];
+}
+
+interface StderrChannel extends ChannelBase {
+  driver: "stderr";
+}
+
+interface ConsoleChannel extends ChannelBase {
+  driver: "console";
+}
+
+type Channel =
+  | SingleChannel
+  | DailyChannel
+  | StackChannel
+  | StderrChannel
+  | ConsoleChannel;
+
+type Channels = Record<string, Channel>;
+
+export interface LogConfig {
+  default: string;
+  channels: Channels;
+}
