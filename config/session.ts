3496a1e80c71a44952fdc590e9c7ebac41675a8f
kyuuukira <genesistroy.fdc@gmail.com>
2025-08-15 18:37:10 +0800

    fix

--- config/session.ts
+++ config/session.ts
@@ -1,7 +1,7 @@
 import { Str } from "Illuminate/Support/index.ts";
 import { SessionConfig } from "configs/@types/index.d.ts";
 const constant: SessionConfig = {
-  driver: "cache",
+  driver: "database",
 
   lifetime: env("SESSION_LIFETIME", 120),
 
