720a0197e0a3c019b03775a9f8c89408b9e91656
kyuuukira <genesistroy.fdc@gmail.com>
2025-09-02 14:14:42 +0800

    todo

--- database/factories/CommentFactory.ts
+++ database/factories/CommentFactory.ts
@@ -7,8 +7,8 @@ export default class CommentFactory extends Factory {
   public definition() {
     return {
       content: this.faker.sentence(),
-      posts_id: this.faker.numberBetween(1, 50), // Assuming post IDs are between 1 and 100
-      users_id: this.faker.numberBetween(1, 10), // Assuming user IDs are between 1 and 100
+      posts_id: this.faker.numberBetween(1, 50),
+      users_id: this.faker.numberBetween(1, 10),
     };
   }
 }
