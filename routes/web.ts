8b30fd6c56c6de22a289a9de6b7491bab9b1112b
kiratrizon <tgenesistroy@gmail.com>
2025-09-19 00:27:24 +0800

    fix

--- routes/web.ts
+++ routes/web.ts
@@ -2,4 +2,9 @@ import { Hash, Route } from "Illuminate/Support/Facades/index.ts";
 import UserController from "App/Http/Controllers/UserController.ts";
 import User from "App/Models/User.ts";
 
-Route.get("/", async () => "hello");
+Route.get("/", async ({request}) => {
+
+    return "Hello World";
+}).middleware(async ({request}, next)=>{
+    return next();
+});
